a643b3d39fad00927672f643c40e666f4ae90873
Lucas Bent <lucas.bent@maine.edu>
2023-10-12 15:48:44 -0400

    add router for pages

---
+++ fe/src/routes/Home.js
@@ -0,0 +1,11 @@
+import './Home.css'
+
+function Home() {
+    return (
+        <span>
+            This is the home page!!
+        </span>
+    )
+}
+
+export default Home;
---
+++ fe/src/routes/Login.js
@@ -0,0 +1,11 @@
+import './Login.css'
+
+function Login() {
+  return (
+    <div id='login-message'>
+      This is the login page!!
+    </div>
+  )
+}
+
+export default Login
