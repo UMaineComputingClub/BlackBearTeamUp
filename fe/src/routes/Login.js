8ed3864c47db8d4ab681a22a34414a6eccdab529
C665967 <corey.kaulenas@maine.edu>
2023-10-18 19:31:26 -0400

    Basic login page

--- fe/src/routes/Login.js
+++ fe/src/routes/Login.js
@@ -1,9 +1,44 @@
 import './Login.css'
+import React, {useState} from 'react';
 
 function Login() {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [loggedIn, setLoggedIn] = useState(false);
+
+  const handleLogin = () => {
+    // Enter admin into both fields to log in
+    if (username === 'admin' && password === 'admin') {
+      setLoggedIn(true);
+    } else {
+      alert('Login failed. Please check your credentials.');
+    }
+  };
   return (
-    <div id='login-message'>
-      This is the login page!!
+    <div className="login-container">
+      <h2>Login</h2>
+      {loggedIn ? (
+        <div>
+          <p>You are logged in!</p>
+          {/* Display a return value or any other content for the logged-in state */}
+        </div>
+      ) : (
+        <div>
+          <input
+            type="text"
+            placeholder="Username"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+          />
+          <input
+            type="password"
+            placeholder="Password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <button onClick={handleLogin}>Login</button>
+        </div>
+      )}
     </div>
   )
 }
