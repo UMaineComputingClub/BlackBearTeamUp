f087ba3d4f2a1819a0e001e89fe54e47f0603a3d
C665967 <corey.kaulenas@maine.edu>
2023-10-19 13:50:16 -0400

    Added tooltips

--- fe/src/routes/Login.js
+++ fe/src/routes/Login.js
@@ -2,15 +2,26 @@ import './Login.css'
 import React, {useState} from 'react';
 
 function Login() {
+  // usernames and passwords
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
+
+  // logged in status
   const [loggedIn, setLoggedIn] = useState(false);
 
+  // Tooltips for usernames and passwords, could put password requirements here
+  const [showUsernameTooltip, setShowUsernameTooltip] = useState(false);
+  const [showPasswordTooltip, setShowPasswordTooltip] = useState(false);
+
   const handleLogin = () => {
-    // Enter admin into both fields to log in
+    // Password checking
+    // Should implement password requirements
+
+    // Should query the database here
     if (username === 'admin' && password === 'admin') {
       setLoggedIn(true);
     } else {
+      // login fail text
       alert('Login failed. Please check your credentials.');
     }
   };
@@ -20,24 +31,38 @@ function Login() {
       <div className="login-container">
         <h2>Login</h2>
         {loggedIn ? (
+          // login success page
           <div>
             <p>You are logged in!</p>
-            {/* Display a return value or any other content for the logged-in state */}
+            {/*Return value for logged in state*/}
           </div>
         ) : (
+          // login page
           <div>
             <input
-              type="text"
-              placeholder="Username"
-              value={username}
-              onChange={(e) => setUsername(e.target.value)}
-            />
-            <input
-              type="password"
-              placeholder="Password"
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
-            />
+            type="text"
+            placeholder="Username"
+            value={username} // change
+            onChange={(e) => setUsername(e.target.value)}
+            onFocus={() => setShowUsernameTooltip(true)}
+            onBlur={() => setShowUsernameTooltip(false)}
+          />
+          {showUsernameTooltip && (
+            // change the tooltip text here
+            <div className="tooltip">Username: 'admin'</div>
+          )}
+          <input
+            type="password"
+            placeholder="Password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            onFocus={() => setShowPasswordTooltip(true)}
+            onBlur={() => setShowPasswordTooltip(false)}
+          />
+            {showPasswordTooltip && (
+              // change the tooltip text here
+              <div className="tooltip">Password: 'admin'</div>
+            )}
             <button onClick={handleLogin}>Login</button>
           </div>
         )}
