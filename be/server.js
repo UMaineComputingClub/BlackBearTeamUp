e4f64e247943426e1c0748bee75871089d0f8d25
Lucas Bent <lucas.bent@maine.edu>
2023-10-19 23:08:24 -0400

    server.js: added test post request

--- be/server.js
+++ be/server.js
@@ -34,11 +34,29 @@ server.listen(Number(process.env.PORT), () => {
     console.log(`listening on *:${process.env.PORT}`)
 })
 
-app.get('/', async (req, res) => {
+app.get('/api/test', async (req, res) => {
     try {
-        return res.status(200).send({ message: "Hello, browser!" })
+        return res.status(200).send({ message: 'Hello, browser!' })
     }
     catch (e) {
+        res.status(400).send({ message: e })
+    }
+})
+
+// here's an example post request implemented on the backend
+app.post('/api/namefun', async (req, res) => {
+    try {
+        // req.body contains the data object passed from the frontend
+
+        // if there is no name property on the recieved object, respond with an error code and corresponding message
+        if (!req.body.name)
+            return res.status(400).send({ message: `Hey, you didn't give me a name!` })
+
+        // otherwise, respond with the 200 OK code, and a nice friendly message!
+        return res.status(200).send({ message: `Hey, nice to meet you! ${req.body.name} is a pretty cool name! I'm pretty sure it has ${req.body.name.length} characters in it.` })
+    }
+    catch (e) {
+        // if something goes horribly wrong, respond about that too
         res.status(400).send({ message: e })
     }
 })
