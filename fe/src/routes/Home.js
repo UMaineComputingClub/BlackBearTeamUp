fba737673962905784732f87f3d12a2aff229b0e
Lucas Bent <lucas.bent@maine.edu>
2023-10-19 23:10:12 -0400

    added an annotated POST request example

--- fe/src/routes/Home.js
+++ fe/src/routes/Home.js
@@ -1,11 +1,40 @@
 import './Home.css'
+// check out Utils.js to see the code for these shorthand HTTP requests (done via the fetch function)
+import { post } from 'Utils.js'
 
 function Home() {
+
+    // marking the function as async allows for "await" statements
+    async function postTest() {
+        const name = prompt(`Hey, what's your name?`)
+
+        // creating a JavaScript object using the data I wanna pass to the server 
+        const requestData = {
+            name: name
+        }
+
+        // wrap the request in a try catch block to protect ourselves
+        try {
+
+            // using the await keyword (with a compatible function) causes the code to stop there until the awaited function is complete (until it resolves its Promise)
+            // here, we are sending a POST request to the "/api/namefun" endpoint, with our requestData object in tow
+            const response = await post('/api/namefun', requestData)
+
+            // once the server has sent a response, the postTest function will continue
+            alert(response.message)
+        }
+        catch (e) {
+            alert(`Error ${e.status}: ${e.message}`)
+        }
+    }
+
     return (
-        <span>
+        <div>
             This is the home page!!
-        </span>
+            <br />
+            <button onClick={postTest}>post request example</button>
+        </div>
     )
 }
 
-export default Home;
+export default Home
