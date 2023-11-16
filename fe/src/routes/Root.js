703ba3bdf10e783d267ae3b2b96cac83a2883e82
Lucas Bent <lucas.bent@maine.edu>
2023-11-16 14:53:56 -0500

    user session

--- fe/src/routes/Root.js
+++ fe/src/routes/Root.js
@@ -4,13 +4,27 @@ import logo from "./images/bear.png"
 import user from "./images/user.png"
 import { useContext } from 'react'
 import { UserContext } from "Session"
-import { getAuth } from "firebase/auth";
+import { auth } from 'firebase.js'
+import { useNavigate } from "react-router-dom"
+import { signOut, onAuthStateChanged } from "firebase/auth"
 
 
 export default function Root() {
 
-    const { username } = useContext(UserContext)
+    const { username, setUsername } = useContext(UserContext)
+    const navigate = useNavigate()
 
+    onAuthStateChanged(auth, function (user) {
+        if (user) {
+            setUsername(user.email)
+        }
+    });
+
+    async function logout() {
+        await signOut(auth)
+        setUsername(null)
+        navigate('/')
+    }
 
     // const getCurrentUser = async function () {
     //         const auth = getAuth();
@@ -31,23 +45,23 @@ export default function Root() {
             <div id='detail'>
 
                 <header>
-                <img src={logo} alt="graphic of a bear" />
-                <h1>BBT</h1>
-                <nav>
-                    <ul>
-                        <li><a href="/">Home</a></li>
-                        <li><a href="/">Search</a></li>
-                        <li><a href="/">Profile</a></li>
-                        <li><a href="/">Logout</a></li>
-                    </ul>
-                </nav>
-                <div id="user">
-                    <img src={user} alt="none" class="round" />
-                    <h4>{username}</h4>
-                    {/* <h4>{getCurrentUser()}</h4> */}
-                </div>
-                <div class="unfloat"></div>
-            </header>
+                    <img src={logo} alt="graphic of a bear" />
+                    <h1>BBT</h1>
+                    <nav>
+                        <ul>
+                            <li><a href="/">Home</a></li>
+                            <li><a href="/">Search</a></li>
+                            <li><a href="/">Profile</a></li>
+                            {!username ? (<li><a href="/login">Login</a></li>) : (<li><button onClick={logout}>Logout</button></li>)}
+                        </ul>
+                    </nav>
+                    <div id="user">
+                        <img src={user} alt="none" class="round" />
+                        <h4>{username}</h4>
+                        {/* <h4>{getCurrentUser()}</h4> */}
+                    </div>
+                    <div class="unfloat"></div>
+                </header>
             </div>
             <Outlet />
         </>
